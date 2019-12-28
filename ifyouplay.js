f823b159e478d88989c768e34d828c542cbe26b4
Naitian Zhou <naitian@naitian.org>
2019-12-28 13:00:56 -0500

    Misc Updates and Improvements

--- ifyouplay.js
+++ ifyouplay.js
@@ -1,27 +1,31 @@
 const presets = {
-  'rewind': {
-    id: 'YbJOTdZBX1g',
-    timestamp: '6:58',
-    blurb: '<a href="https://twitter.com/fab_nz/status/1079555364759031808">YouTube Rewind 2018, Will Smith will say "uhhhh that\'s hot" at exactly midnight.</a>'
+  rewind: {
+    id: "YbJOTdZBX1g",
+    timestamp: "6:58",
+    blurb:
+      '<a href="https://twitter.com/fab_nz/status/1079555364759031808">YouTube Rewind 2018, Will Smith will say "uhhhh that\'s hot" at exactly midnight.</a>'
   },
-  'ddu': {
-    id: 'IHNzOHi8sJs',
-    timestamp: '1:18',
-    blurb: '<a href="https://twitter.com/black2dpink824/status/1079676880746356737">Ddu-Du Ddu-Du, BLACKPINK will hit you with that Ddu-du Ddu-du just as 2019 begins</a>'
+  ddu: {
+    id: "IHNzOHi8sJs",
+    timestamp: "1:18",
+    blurb:
+      '<a href="https://twitter.com/black2dpink824/status/1079676880746356737">Ddu-Du Ddu-Du, BLACKPINK will hit you with that Ddu-du Ddu-du just as 2019 begins</a>'
   },
-  'abba': {
-    id: '3Uo0JAUWijM',
-    timestamp: '0:46',
-    blurb: '<a href="https://twitter.com/morganllaughlin/status/1079695056993468416">Happy New Year by ABBA, ABBA will be the first people to tell you happy new year</a>'
+  abba: {
+    id: "3Uo0JAUWijM",
+    timestamp: "0:46",
+    blurb:
+      '<a href="https://twitter.com/morganllaughlin/status/1079695056993468416">Happy New Year by ABBA, ABBA will be the first people to tell you happy new year</a>'
   },
-  'umich': {
-    id: 'pIKl1NdR3rk',
-    timestamp: '01:25',
-    blurb: 'the Michigan Transportation Musical, Billy Magic will introduce himself at midnight.'
+  umich: {
+    id: "pIKl1NdR3rk",
+    timestamp: "01:25",
+    blurb:
+      "the Michigan Transportation Musical, Billy Magic will introduce himself at midnight."
   }
-}
+};
 
-const GOTIME = new Date('January 1, 2020 00:00:00');
+const GOTIME = new Date("January 1, 2020 00:00:00");
 
 var player;
 var vid;
@@ -29,15 +33,15 @@ var video_countdown = false;
 var showing_presets = false;
 
 function initYoutubePlayerAPI() {
-  var tag = document.createElement('script');
+  var tag = document.createElement("script");
 
   tag.src = "//www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
+  var firstScriptTag = document.getElementsByTagName("script")[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
 function parseTimestampIntoSeconds(timestamp) {
-  const parts = timestamp.split(':');
+  const parts = timestamp.split(":");
   let hours, minutes, seconds;
   if (parts.length == 2) {
     hours = 0;
@@ -53,12 +57,13 @@ function parseTimestampIntoSeconds(timestamp) {
 }
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
+  const container = document.querySelector(".vid-container");
+  player = new YT.Player("player", {
+    height: container.clientHeight,
+    width: container.clientWidth,
     videoId: vid.id,
     events: {
-      'onReady': onPlayerReady,
+      onReady: onPlayerReady
       // 'onStateChange': onPlayerStateChange
     }
   });
@@ -73,15 +78,15 @@ function playVideoIfTime() {
   //        This is not ideal.
   let seconds = parseTimestampIntoSeconds(vid.timestamp) + 1;
   let delta = parseInt((GOTIME - new Date()) / 1000);
-  console.log('delta: ', delta);
-  console.log('seconds: ', seconds);
+  console.log("delta: ", delta);
+  console.log("seconds: ", seconds);
   if (delta > 0 && delta <= seconds) {
     player.seekTo(seconds - delta);
     player.playVideo();
-    console.log('Played Video');
+    console.log("Played Video");
   } else {
     if (delta < 0) return;
-    console.log('Waiting', (delta - seconds) * 0.5, 'seconds');
+    console.log("Waiting", (delta - seconds) * 0.5, "seconds");
     window.setTimeout(playVideoIfTime, (delta - seconds) * 500);
   }
 }
@@ -91,23 +96,27 @@ function renderCountdown() {
   if (video_countdown) {
     remaining -= parseTimestampIntoSeconds(vid.timestamp);
   }
-  remaining = remaining < 0 ? 60 * 60 * 24 - remaining: remaining;
+  remaining = remaining < 0 ? 60 * 60 * 24 - remaining : remaining;
 
   var date = new Date(null);
   date.setSeconds(remaining);
   var timeString = date.toISOString().substr(11, 8);
 
   if (remaining < 24 * 60 * 60) {
-    document.querySelector('.clock').innerText = timeString;
+    document.querySelector(".clock").innerText = timeString;
+  } else if (remaining < 7 * 24 * 60 * 60) {
+    document.querySelector(".clock").innerText = "Less Than A Week";
+  } else if (remaining < 30 * 24 * 60 * 60) {
+    document.querySelector(".clock").innerText = "Less Than A Month";
   } else {
-    document.querySelector('.clock').innerText = "A Long Freaking Time";
+    document.querySelector(".clock").innerText = "A Long Freaking Time";
   }
   window.setTimeout(renderCountdown, 500);
 }
 
 function ringInTheNewYear(vid) {
   if (vid.blurb) {
-    let blurb = document.querySelector('.blurb');
+    let blurb = document.querySelector(".blurb");
     blurb.innerHTML = vid.blurb;
   }
   initYoutubePlayerAPI();
@@ -116,25 +125,25 @@ function ringInTheNewYear(vid) {
 
 function generatePresetList() {
   let url = `${window.location.origin}${window.location.pathname}`;
-  let result = '';
+  let result = "";
   for (let name in presets) {
     let href = `${url}?preset=${name}`;
     let a = `<a href=${href}>${name}</a> `;
     result += a;
   }
-  return result + ' | ';
+  return result + " | ";
 }
 
-function handlePresets(e) {
-  console.log('click');
-  let presets_container = document.querySelector('.presets');
-  let presets_button = document.querySelector('.show-presets');
+function handlePresets() {
+  console.log("click");
+  let presets_container = document.querySelector(".presets");
+  let presets_button = document.querySelector(".show-presets");
   if (showing_presets) {
-    presets_container.innerHTML = '';
-    presets_button.innerText = 'presets';
+    presets_container.innerHTML = "";
+    presets_button.innerText = "presets";
   } else {
     presets_container.innerHTML = generatePresetList();
-    presets_button.innerText = 'hide';
+    presets_button.innerText = "hide";
   }
   showing_presets = !showing_presets;
 }
@@ -143,33 +152,36 @@ function handleClock(e) {
   let clock_button = e.target;
   video_countdown = !video_countdown;
   if (video_countdown) {
-    clock_button.innerText = 'until video plays';
+    clock_button.innerText = "until video plays";
   } else {
-    clock_button.innerText = 'until 2019!';
+    clock_button.innerText = "until 2019!";
   }
   renderCountdown();
 }
 
-window.onload = function () {
-  console.log('Loaded');
+window.onload = function() {
+  console.log("Loaded");
 
-  let presets_button = document.querySelector('.show-presets');
+  let presets_button = document.querySelector(".show-presets");
   presets_button.onclick = handlePresets;
-  let clock_button = document.querySelector('.clock-label');
+  let clock_button = document.querySelector(".clock-label");
   clock_button.onclick = handleClock;
 
   let url = new URL(window.location.href);
-  if (url.searchParams.get('custom')) {
-    const id = url.searchParams.get('id');
-    const timestamp = url.searchParams.get('timestamp');
+  if (url.searchParams.get("custom")) {
+    const id = url.searchParams.get("id");
+    const timestamp = url.searchParams.get("timestamp");
     vid = {
       id,
       timestamp
-    }
+    };
   } else {
-    vid = presets[url.searchParams.get('preset')];
-    if (!vid) console.error('Preset not found: ', url.searchParams.get('preset'));
+    vid = presets[url.searchParams.get("preset")];
+    if (!vid)
+      console.error("Preset not found: ", url.searchParams.get("preset"));
+    const items = Object.keys(presets);
+    vid = presets[items[Math.floor(Math.random() * items.length)]];
   }
 
   ringInTheNewYear(vid);
-}
+};
